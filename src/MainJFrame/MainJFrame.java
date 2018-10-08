5b81b80aadf231beca1531718f33750ef123f1f7
FanZeng <fanzengau@gmail.com>
2018-10-08 11:49:30 +1000

    restructured dirs

--- src/MainJFrame/MainJFrame.java
+++ src/MainJFrame/MainJFrame.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MainJFrame;
 
 import java.util.*;
 
-/**
- *
- * @author Gloria
- */
 public class MainJFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form MainJFrame
-     */
     public MainJFrame() {
         initComponents();
     }
