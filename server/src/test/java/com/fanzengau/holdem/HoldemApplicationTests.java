96ba585ed66dc3cc136577a78b295d206aa687ad
Fan Zeng <fanzengau@gmail.com>
2023-10-06 12:06:33 +1000

    update build.gradle

--- server/src/test/java/com/fanzengau/holdem/HoldemApplicationTests.java
+++ server/src/test/java/com/fanzengau/holdem/HoldemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.fanzengau.holdem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HoldemApplicationTests {
 
 	@Test
 	void contextLoads() {
