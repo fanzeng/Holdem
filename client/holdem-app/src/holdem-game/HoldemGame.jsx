e734ac127bc3092edfc737f8020770d341741d2b
Fan Zeng <fanzengau@gmail.com>
2024-01-14 17:58:39 +1000

    update debounce logic

--- client/holdem-app/src/holdem-game/HoldemGame.jsx
+++ client/holdem-app/src/holdem-game/HoldemGame.jsx
@@ -154,6 +154,7 @@ export function HoldemGame() {
   }, [holdemState])
 
   const onPlayerBet = (id, betValue, isFold = false) => {
+    console.log('clicked!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     fetch(`${serverAddr}/player-bet?gameSessionId=${gameSessionId}`, {
       method: 'POST',
       headers: {
