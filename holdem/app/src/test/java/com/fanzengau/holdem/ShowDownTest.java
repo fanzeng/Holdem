e0fa6faf2e706cdc2b52bdb26a1034a3d7c3d0f1
Fan Zeng <fanzengau@gmail.com>
2024-01-17 18:56:39 +1000

    refactor

--- holdem/app/src/test/java/com/fanzengau/holdem/ShowDownTest.java
+++ holdem/app/src/test/java/com/fanzengau/holdem/ShowDownTest.java
@@ -12,51 +12,50 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShowDownTest {
 
-    @Test
-    void getShowDownResultFlush() {
+    void testShowDownResult(
+        String[] privateCard, String[] board, Holdem.BEST_HAND_TYPE expectedBestHandType, Card[] expectedKickers
+    ) {
         var showDown = new ShowDown();
         var player = new Player(0);
-        player.privateCard = new String[]{"4H", "KH"};
+        player.privateCard = privateCard;
+        var showDownResult = showDown.getShowDownResult(player, board);
+        assertEquals(expectedBestHandType, showDownResult.bestHandType);
+        assertTrue(Arrays.equals(expectedKickers, showDownResult.getKickers()));
+    }
+
+    @Test
+    void getShowDownResultFlush() {
+        var privateCard = new String[]{"4H", "KH"};
         var board = new String[]{"2H", "TH", "5H", "4C", "7C"};
         var expectedBestHandType = FLUSH;
         var expectedKickers = new Card[]{
             new Card("KH"), new Card("TH"),
             new Card("5H"), new Card("4H"), new Card("2H")
         };
-        var showDownResult = showDown.getShowDownResult(player, board);
-        assertEquals(expectedBestHandType, showDownResult.bestHandType);
-        assertTrue(Arrays.equals(expectedKickers, showDownResult.getKickers()));
+        testShowDownResult(privateCard, board, expectedBestHandType, expectedKickers);
     }
 
     @Test
     void getShowDownResultStraightAceHigh() {
-        var showDown = new ShowDown();
-        var player = new Player(0);
-        player.privateCard = new String[]{"AS", "KS"};
+        var privateCard = new String[]{"AS", "KS"};
         var board = new String[]{"TH", "QC", "JD", "4C", "7C"};
         var expectedBestHandType = STRAIGHT;
         var expectedKickers = new Card[]{
                 new Card("AS"), new Card("KS"),
                 new Card("QC"), new Card("JD"), new Card("TH")
         };
-        var showDownResult = showDown.getShowDownResult(player, board);
-        assertEquals(expectedBestHandType, showDownResult.bestHandType);
-        assertTrue(Arrays.equals(expectedKickers, showDownResult.getKickers()));
+        testShowDownResult(privateCard, board, expectedBestHandType, expectedKickers);
     }
 
     @Test
     void getShowDownResultStraightAceLow() {
-        var showDown = new ShowDown();
-        var player = new Player(0);
-        player.privateCard = new String[]{"AS", "KS"};
+        var privateCard = new String[]{"AS", "KS"};
         var board = new String[]{"2H", "3C", "5D", "4C", "7C"};
         var expectedBestHandType = STRAIGHT;
         var expectedKickers = new Card[]{
                 new Card("5D"), new Card("4C"),
                 new Card("3C"), new Card("2H"), new Card("AS")
         };
-        var showDownResult = showDown.getShowDownResult(player, board);
-        assertEquals(expectedBestHandType, showDownResult.bestHandType);
-        assertTrue(Arrays.equals(expectedKickers, showDownResult.getKickers()));
+        testShowDownResult(privateCard, board, expectedBestHandType, expectedKickers);
     }
 }
