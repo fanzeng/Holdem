4ef4368e02135512d57d56f210790647451f93a9
Fan Zeng <fanzengau@gmail.com>
2025-04-28 13:49:05 +1000

    Try improve startup speed

--- server/src/main/java/com/fanzengau/holdemservice/HoldemServiceApplication.java
+++ server/src/main/java/com/fanzengau/holdemservice/HoldemServiceApplication.java
@@ -3,11 +3,22 @@ package com.fanzengau.holdemservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.JvmMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.LogbackMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {
+	JacksonAutoConfiguration.class,
+	MetricsAutoConfiguration.class,
+	LogbackMetricsAutoConfiguration.class,
+	MetricsAutoConfiguration.class
+})
 public class HoldemServiceApplication {
 
 	public static void main(String[] args) {
